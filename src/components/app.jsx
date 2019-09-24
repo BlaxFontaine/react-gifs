806f909b887221618d5a809722c38abe7018050b
Blax Fontaine <valerie.gehle@gmail.com>
2019-09-24 23:56:43 +0200

    select gif

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import ReactDOM from 'react-dom';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "WQxkpI7LTStUExhrR7"
+    };
+
+  }
+
+  search = (query) => {
+    giphy("rorNDyivxYUXgP1DLP67hxJZCpeg1ngh").search({
+      q: query,
+      rating: 'g'
+    }, (err, res) => {
+      this.setState({gifs: res.data});
+    });
+
+  }
+
+  gifClick = (id) => {
+    this.setState({ selectedGifId: id});
+  }
+
   render() {
-    const gifs = [
-      { id: "k1SuVPEuA89S8" },
-      { id: "WQxkpI7LTStUExhrR7" }
-    ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search = {this.search}/>
           <div className="selected-gif">
-            <Gif id="FrnpqArQZtti8"/>
+            <Gif id = {this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs = {gifs} />
+          <GifList gifs = {this.state.gifs} gifClick = {this.gifClick}/>
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleClick = () => {
+    if (this.props.gifClick) {
+      this.props.gifClick(this.props.id);
+    }
+  }
+
   render() {
     const src = `https://media2.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
-      <img className = "gif" src={src} alt=""/>
+      <img className = "gif" src={src} alt="" onClick={this.handleClick}/>
       );
   }
 }
