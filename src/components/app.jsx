943fef7c99b51813026a8b54f1f173f48a1152ee
Blax Fontaine <valerie.gehle@gmail.com>
2019-09-24 17:20:26 +0200

    giflist

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: "k1SuVPEuA89S8" },
+      { id: "WQxkpI7LTStUExhrR7" }
+    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif />
+            <Gif id="FrnpqArQZtti8"/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs = {gifs} />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = "https://media2.giphy.com/media/FrnpqArQZtti8/giphy.gif?cid=790b7611408f4757a9ac354a777d947a41fb65f0da21c728&rid=giphy.gif";
+    const src = `https://media2.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
       <img className = "gif" src={src} alt=""/>
       );
